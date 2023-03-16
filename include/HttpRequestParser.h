9c3a9a3d29824dba251f94c3d7f22c497efc342d
skystriker1997 <k77705@protonmail.com>
2023-03-16 15:34:48 +1100

    add this class to parse http request and store parsed output

---
+++ include/HttpRequestParser.h
@@ -0,0 +1,35 @@
+#include <regex>
+#include <string>
+
+
+#ifndef SKYCPPSERVER_HTTPPARSER_H
+#define SKYCPPSERVER_HTTPPARSER_H
+
+
+class HttpRequestParser {
+private:
+    std::string http_request_;
+    bool problematic_;
+    std::string method_;
+    std::string uri_;
+    std::string content_type_;
+    std::string content_length_;
+    std::string request_body_;
+
+public:
+    HttpRequestParser(const char* request);
+
+    ~HttpRequestParser();
+
+    void Parse();
+
+    bool CheckProblematic();
+
+    const std::string& GetMethod();
+    const std::string& GetURI();
+    const std::string& GetContentType();
+    const std::string& GetContentLength();
+    const std::string& GetBody();
+};
+
+#endif //SKYCPPSERVER_HTTPPARSER_H
